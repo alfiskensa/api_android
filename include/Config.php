e56ca59e0fcc4a67e59bbedfa9b764c7cfe8fab9
alfiskensa <vincentalfi@gmail.com>
2016-09-04 11:33:27 +0700

    Initial Commit

---
+++ include/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "ap-cdbr-azure-southeast-b.cloudapp.net");
+define("DB_PORT", "3306")
+define("DB_USER", "b48a56a769b0a7");
+define("DB_PASSWORD", "413edfe1");
+define("DB_DATABASE", "android_api");
+?>
